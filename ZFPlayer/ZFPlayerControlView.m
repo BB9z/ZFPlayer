9a03dbbc6aae51d1e4edd0974c7ced5efe2fbb53
BB9z <bb9z@me.com>
2016-08-05 10:02:04 +0800

    清理

--- ZFPlayer/ZFPlayerControlView.m
+++ ZFPlayer/ZFPlayerControlView.m
@@ -26,28 +26,11 @@
 #import "RFTimer.h"
 @import MediaPlayer;
 
-static const CGFloat ZFPlayerAnimationTimeInterval             = 7.0f;
-static const CGFloat ZFPlayerControlBarAutoFadeOutTimeInterval = 0.5f;
 
-// 枚举值，包含水平移动方向和垂直移动方向
-typedef NS_ENUM(NSInteger, PanDirection){
-    PanDirectionHorizontalMoved, //横向移动
-    PanDirectionVerticalMoved    //纵向移动
-};
+static const CGFloat ZFPlayerControlBarAutoFadeOutTimeInterval = 0.5f;
 
 @interface ZFPlayerControlView ()
 @property (nonatomic, strong) RFTimer *autoHidePanelTimer;
-
-/** 定义一个实例变量，保存枚举值 */
-@property (nonatomic, assign) PanDirection panDirection;
-
-/** 是否在调节音量*/
-@property (nonatomic, assign) BOOL isVolume;
-
-/** 用来保存快进的总时长 */
-@property (nonatomic, assign) NSTimeInterval sumTime;
-
-@property (nonatomic) BOOL playbackProgressChanging;
 @property (nonatomic) double seekBeginValue;
 @end
 
@@ -228,140 +211,6 @@ RFInitializingRootForUIView
             [self.activity stopAnimating];
         }
     }
-}
-
-#pragma mark -
-
-- (void)panDirection:(UIPanGestureRecognizer *)pan {
-    //根据在view上Pan的位置，确定是调音量还是亮度
-    CGPoint locationPoint = [pan locationInView:self];
-
-    // 我们要响应水平移动和垂直移动
-    // 根据上次和本次移动的位置，算出一个速率的point
-    CGPoint veloctyPoint = [pan velocityInView:self];
-
-    // 判断是垂直移动还是水平移动
-    switch (pan.state) {
-        case UIGestureRecognizerStateBegan:{ // 开始移动
-            // 使用绝对值来判断移动的方向
-            CGFloat x = fabs(veloctyPoint.x);
-            CGFloat y = fabs(veloctyPoint.y);
-            if (x > y) { // 水平移动
-                self.panDirection           = PanDirectionHorizontalMoved;
-                // 取消隐藏
-                self.horizontalLabel.hidden = NO;
-                // 给sumTime初值
-                CMTime time = self.player.playerItem.currentTime;
-                self.sumTime = time.value/time.timescale;
-            }
-            else if (x < y){ // 垂直移动
-                self.panDirection = PanDirectionVerticalMoved;
-                // 开始滑动的时候,状态改为正在控制音量
-                if (locationPoint.x > self.bounds.size.width / 2) {
-                    self.isVolume = YES;
-                }
-                else { // 状态改为显示亮度调节
-                    self.isVolume = NO;
-                }
-            }
-            break;
-        }
-        case UIGestureRecognizerStateChanged:{ // 正在移动
-            switch (self.panDirection) {
-                case PanDirectionHorizontalMoved:{
-                    [self horizontalMoved:veloctyPoint.x]; // 水平移动的方法只要x方向的值
-                    break;
-                }
-                case PanDirectionVerticalMoved:{
-                    [self verticalMoved:veloctyPoint.y]; // 垂直移动方法只要y方向的值
-                    break;
-                }
-                default:
-                    break;
-            } // END self.panDirection switch
-            break;
-        }
-        case UIGestureRecognizerStateEnded:{ // 移动停止
-            // 移动结束也需要判断垂直或者平移
-            // 比如水平移动结束时，要快进到指定位置，如果这里没有判断，当我们调节音量完之后，会出现屏幕跳动的bug
-            switch (self.panDirection) {
-                case PanDirectionHorizontalMoved:{
-
-                    // 继续播放
-                    [self.player play];
-
-                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                        // 隐藏视图
-                        self.horizontalLabel.hidden = YES;
-                    });
-                    //快进、快退时候把开始播放按钮改为播放状态
-                    self.startPauseButton.selected = YES;
-                    self.player.paused = NO;
-
-                    [self.player seekToTime:self.sumTime completion:^(BOOL finished) {
-                        if (self.player.paused) {
-                            return;
-                        }
-                        [self.player play];
-                    }];
-                    self.sumTime = 0;
-                    break;
-                }
-                case PanDirectionVerticalMoved:{
-                    // 垂直移动结束后，把状态改为不再控制音量
-                    self.isVolume = NO;
-                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                        self.horizontalLabel.hidden = YES;
-                    });
-                    break;
-                }
-                default:
-                    break;
-            } // END self.panDirection switch
-            break;
-        }
-        default:
-            break;
-    } // END pan.state switch
-}
-
-- (void)verticalMoved:(CGFloat)value {
-    if (self.isVolume) {
-        // TODO: 暴漏 AVPlayer 直接修改音量
-        MPMusicPlayerController *vpc = [MPMusicPlayerController applicationMusicPlayer];
-        vpc.volume = value / 10000; // 越小幅度越小
-    }
-    else {
-        //亮度
-        [UIScreen mainScreen].brightness -= value / 10000;
-    }
-}
-
-- (void)horizontalMoved:(CGFloat)value {
-    // 快进快退的方法
-    NSString *style = @"";
-    if (value < 0) {
-        style = @"<<";
-    }
-    else if (value > 0){
-        style = @">>";
-    }
-
-    // 每次滑动需要叠加时间
-    self.sumTime += value / 200;
-
-    // 需要限定sumTime的范围
-    CMTime totalTime = self.player.playerItem.duration;
-    CGFloat totalMovieDuration = (CGFloat)totalTime.value/totalTime.timescale;
-    if (self.sumTime > totalMovieDuration) {
-        self.sumTime = totalMovieDuration;
-    }else if (self.sumTime < 0){
-        self.sumTime = 0;
-    }
-
-    NSString *nowTime = [self durationMSStringWithTimeInterval:self.sumTime];
-    NSString *durationTime = [self durationMSStringWithTimeInterval:totalMovieDuration];
-    self.horizontalLabel.text = [NSString stringWithFormat:@"%@ %@ / %@",style, nowTime, durationTime];
 }
 
 #pragma mark -
